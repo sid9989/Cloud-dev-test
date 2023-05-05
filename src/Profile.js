e2d7bd6095ca841e8acabbdf857890fc2fe859b6
SidharthaG <sidhartha.r.gundarapu@pwc.com>
2023-05-05 13:05:09 +0530

    border done

--- src/Profile.js
+++ src/Profile.js
@@ -3,9 +3,9 @@ import './Profile.css';
 const Profile = ({...props}) =>{
     console.log(props.employee)
     return(
-        <div >
+        <div style={{marginTop: "50px" , padding: "30px 80px" ,  border: "3px solid #EE4E34" , borderRadius: "10px" , backgroundColor:"#FCEDDA"}}>
             <div>
-                Profile
+                <h3 style={{marginTop: "0px"}}>Profile</h3>
             </div>
             <div className="Profile-row">
                 <div style={{width: "45%"}}>first name</div> 
